dc41b3b288e18a600c3f7f9d37fcaa80f3c694f3
Eric Chao <hon-leung-eric.chao@polymtl.ca>
2019-06-10 23:07:01 -0400

    removed name property of modal Trip

--- app/backend/api/models/Trip.js
+++ app/backend/api/models/Trip.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-  name: {type: String},
   departure: { type: String },
   destination: { type: String },
   fake: { type: Boolean, default: false },

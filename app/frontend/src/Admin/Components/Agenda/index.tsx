85242812636875d5a4f89169319eb8b810f304b6
eric <eric.chao@live.fr>
2019-06-29 15:37:26 -0400

    back to mock and removed the <br/> of tickets columns's name

--- app/frontend/src/Admin/Components/Agenda/index.tsx
+++ app/frontend/src/Admin/Components/Agenda/index.tsx
@@ -89,13 +89,13 @@ const Agenda: React.SFC<IProps> = ({
           <th className="spon-agenda__cell spon-agenda__cell--head">From</th>
           <th className="spon-agenda__cell spon-agenda__cell--head">To</th>
           <th className="spon-agenda__cell spon-agenda__cell--head">
-            Qty<br/>of tickets
+            Qty of tickets
           </th>
           <th className="spon-agenda__cell spon-agenda__cell--head">
             Available tickets
           </th>
           <th className="spon-agenda__cell spon-agenda__cell--head">
-            Sold<br/>tickets
+            Sold tickets
           </th>
           <th className="spon-agenda__cell spon-agenda__cell--head">Type</th>
           <th className="spon-agenda__cell spon-agenda__cell--head">

58004a32ecf5b9082067a7e7c8d2e7f43cf85db3
eric <eric.chao@live.fr>
2019-07-11 11:48:05 -0400

    code review

--- app/frontend/src/Admin/Containers/Trips/index.tsx
+++ app/frontend/src/Admin/Containers/Trips/index.tsx
@@ -540,17 +540,17 @@ class TripsContainer extends React.Component<
       modal: { type: modalType, heading: modalHeading }
     } = this.state
 
-    if (filtersFrom.length > 0 || filtersTo.length > 0 ) {
+    if (filtersFrom.length > 0 || filtersTo.length > 0 ) { // filtersFrom.length > 0 peut être remplacé par filtersFrom.length
       let filteredTrips: ITrip[] = [];
       for(let tripIndex: number = 0; tripIndex < results.length; tripIndex++){
-        if(filtersFrom.length > 0) {
+        if(filtersFrom.length > 0) { // ouais encore filtersFrom.length
           for(let index: number = 0; index < filtersFrom.length; index++){
             if(results[tripIndex].departure.toLowerCase() == filtersFrom[index].toLowerCase()) {
               filteredTrips.push(results[tripIndex]);
             }
           }
         }
-        if(filtersTo.length > 0) {
+        if(filtersTo.length > 0) { // ouais encore filtersTo.length
           for(let index: number = 0; index < filtersTo.length; index++){
             if(results[tripIndex].destination.toLowerCase() == filtersTo[index].toLowerCase() && filteredTrips.includes(results[tripIndex]) == false) {
              filteredTrips.push(results[tripIndex]);

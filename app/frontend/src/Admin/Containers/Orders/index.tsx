b6e38ed622407fe448002d100083a92cd9f375f3
philippe <francois-philippe.ossim-belias@polymtl.ca>
2019-07-16 12:01:02 -0400

    correction of bugon the order page

--- app/frontend/src/Admin/Containers/Orders/index.tsx
+++ app/frontend/src/Admin/Containers/Orders/index.tsx
@@ -39,6 +39,7 @@ class OrdersContainer extends React.Component<IProps, IState> {
     
     getOrders(page, 1000, token, sort)
     .then(({ data }) =>{
+      console.log(data.results)
       this.setState({
         isLoading: false,
         results: data.results,

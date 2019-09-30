b7120e9eb257aec70db4096922417fb093442a33
jaykant <jaykant@enginesoft.in>
2019-10-01 00:00:30 +0530

    Tasks https://sponteous.atlassian.net/browse/SMS-418

--- app/frontend/src/App/Components/Preloader/index.tsx
+++ app/frontend/src/App/Components/Preloader/index.tsx
@@ -1,38 +1,46 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+// import { withRouter } from 'react-router-dom'
 
 import MainBlock from '../MainBlock'
 import Title from '../Title'
-import DiceGif from '../../Utils/Media/dice.gif'
+// import LoadingGif from '../../Utils/Media/loading.gif'
 
 import { ISelectedData } from '../../Utils/appTypes'
 import { IProps } from './types'
 import './styles.scss'
 
-const LinkButton = withRouter(({ history }) => (
-  <button
-    children={['CANCEL']}
-    className="preloader-cancel"
-    onClick={() => history.push('/destinations/payment')}
-  />
-))
+// const LinkButton = withRouter(({ history }) => (
+//   <button
+//     children={['CANCEL']}
+//     className="preloader-cancel"
+//     onClick={() => history.push('/destinations/payment')}
+//   />
+// ))
 
 const Preloader: React.SFC<IProps> = ({ selected }) => (
   <MainBlock className="preloader">
-    <div className="preloader-dice">
-      <img src={DiceGif} alt="" />
-    </div>
+    {/* <div className="preloader-dice"> */}
+      <div className="preloader-spon-loader">
+        <div className="preloader-spon-loader__backdrop" />
+        <div className="preloader-spon-loader__container">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    {/* </div> */}
     <Title
       className="preloader-title"
       selected={['Sponteous']}
-      text="Are you ready for your Sponteous trip?"
+      text="Get ready to discover your Sponteous trip!"
     />
     <div className="preloader-thumbnails">
       {selected.map((item: ISelectedData) => (
         <img key={item.tripId} src={item.photo} alt={item.destination.name} />
       ))}
     </div>
-    <LinkButton />
+    {/* <LinkButton /> */}
   </MainBlock>
 )
 

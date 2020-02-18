f57afd6a66b9d2edcdb05dd532206b7421a88167
endmaster0809 <endmaster0809@outlook.com>
2020-02-17 19:37:56 -0800

    fix issues on map ui

--- app/frontend/src/App/Containers/Select/index.tsx
+++ app/frontend/src/App/Containers/Select/index.tsx
@@ -50,6 +50,183 @@ import markerSelected from '../../Utils/Media/marker-selected.png'
 
 const MAX = 5
 
+const mapStyle = [
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#e9e9e9'
+      },
+      {
+        lightness: 17
+      }
+    ]
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5'
+      },
+      {
+        lightness: 20
+      }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#ffffff'
+      },
+      {
+        lightness: 17
+      }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#ffffff'
+      },
+      {
+        lightness: 29
+      },
+      {
+        weight: 0.2
+      }
+    ]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff'
+      },
+      {
+        lightness: 18
+      }
+    ]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff'
+      },
+      {
+        lightness: 16
+      }
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5'
+      },
+      {
+        lightness: 21
+      }
+    ]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dedede'
+      },
+      {
+        lightness: 21
+      }
+    ]
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'on'
+      },
+      {
+        color: '#ffffff'
+      },
+      {
+        lightness: 16
+      }
+    ]
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        saturation: 36
+      },
+      {
+        color: '#333333'
+      },
+      {
+        lightness: 40
+      }
+    ]
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f2f2f2'
+      },
+      {
+        lightness: 19
+      }
+    ]
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#fefefe'
+      },
+      {
+        lightness: 20
+      }
+    ]
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#fefefe'
+      },
+      {
+        lightness: 17
+      },
+      {
+        weight: 1.2
+      }
+    ]
+  }
+]
+
 class SelectContainer extends Component<
   RouteComponentProps<{}> & MapProps & IProps,
   IState
@@ -493,6 +670,19 @@ class SelectContainer extends Component<
     // this.props.updateSelected(bookedTrips)
     this.props.history.push('/destinations/editSelection')
   }
+
+  _mapLoaded(mapProps: any, map: any) {
+    map.setOptions({
+      styles: mapStyle
+    })
+  }
+  mapClicked = () => {
+    console.log('mapclicked')
+    this.setState({
+      showingInfoWindow: false
+    })
+  }
+
   render() {
     const { isCalendarOpen, filters, trips } = this.state
     const { isMax, quantity, selected, departure } = this.props
@@ -719,10 +909,12 @@ class SelectContainer extends Component<
                 lat: lat,
                 lng: lng
               }}
-              zoomControl={false}
+              // zoomControl={false}
               mapTypeControl={false}
               fullscreenControl={false}
-              scaleControl={false}>
+              scaleControl={false}
+              onReady={(mapProps, map) => this._mapLoaded(mapProps, map)}
+              onClick={this.mapClicked}>
               {locations.map((location: any, index: number) => {
                 return (
                   <Marker

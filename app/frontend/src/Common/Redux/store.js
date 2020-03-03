7504ba50206946673e77da5d4f8a36ec43671959
endmaster0809 <endmaster0809@outlook.com>
2020-03-03 14:53:03 -0800

    SMS-495 done

--- app/frontend/src/Common/Redux/store.js
+++ app/frontend/src/Common/Redux/store.js
@@ -2,13 +2,36 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import { rootReducer } from './rootReducer'
+import { timingSafeEqual } from 'crypto'
 
-const initialState = {}
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.log(e)
+    return undefined
+  }
+}
+
+const persistedState = loadFromLocalStorage()
 
 const store = createStore(
   rootReducer,
-  initialState,
+  persistedState,
   composeWithDevTools(applyMiddleware(thunkMiddleware))
 )
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store
